52d35cbe586190c6dfb4604ff2c37f8fa53e3171
Electro-Corp <chinmaytanmay@outlook.com>
2022-08-06 08:09:51 -0600

    Add files via upload

--- drone-project/connection/client.py
+++ drone-project/connection/client.py
@@ -3,7 +3,8 @@ import socket
 HOST = ''
 PORT = 8080
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'REAL????')
-    data = s.recv(1024)
-print('Received', repr(data))
+    while True:
+	    s.connect((HOST, PORT))
+	    s.sendall(b'REAL????')
+	    data = s.recv(1024)
+	    print('Received', repr(data))
