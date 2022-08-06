fe9f3c4bea76f0344dcd28da89138cc93e798b1a
Electro <chinmaytanmay@outlook.com>
2022-08-06 08:13:42 -0600

    fix and stuff

--- drone-project/connection/client.py
+++ drone-project/connection/client.py
@@ -1,10 +1,11 @@
 import socket
 
-HOST = ''
+HOST = '192.168.86.34'
 PORT = 8080
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
     while True:
-	    s.connect((HOST, PORT))
+
 	    s.sendall(b'REAL????')
 	    data = s.recv(1024)
 	    print('Received', repr(data))
