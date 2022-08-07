fde181458be8062a007255ab282141710a9af21d
Electro-Corp <chinmaytanmay@outlook.com>
2022-08-07 15:27:24 -0600

    Multiple Images will be taken so you can process
    Multiple Images will be taken so you can process as video

--- drone-project/connection/server.py
+++ drone-project/connection/server.py
@@ -1,8 +1,8 @@
 import socket
 import os
-
+import time
 HOST = ''
-PORT = 8080
+PORT = 6000
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.bind((HOST,PORT))
 	s.listen(1)
@@ -10,43 +10,46 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	#data = conn.recv(350000)
 	#prevdata = data
 	isboi = 0
-
-	os.system("rm recived.jpg")
+	try:
+		os.system("rm recived.jpg")
+	except:
+		pass
 	
-	with conn:
-		print("Connection: ",addr)
+	#with conn:
+	frames = 0
+	while True:
+		#print("Connection: ",addr)
 		while True:
 			
-			print("reciving")
+			#print("reciving")
 			data = conn.recv(350000)
+			#conn.sendall(bytearray(data))
 			#data = data.decode()
 			
 			#while not data:
 		#		data = conn.recv(10000)
-			print("done")
-			if not data: break
-			print("writing")
+			#print("done")
+			if not data:
+				frames = 0 
+				break
+			#print("writing")
 			#os.system("rm recived.jpg")
 			b = bytearray(data)
 			i = 0
-			if(isboi == 0):
-				pass
-				#while(bytearray[i] != 0xff):
-					
-					
-			with open('recived.jpg','ab') as r:
-				#if(prevdata != data and isboi == 1):
-					
+
+			if(data == bytearray([0xf8])):
+				print("NEXT FRAME")
+				frames = frames + 1
+				break		
+			
+			with open('images/recived'+str(frames)+'.jpg','ab') as r:
+					#if(prevdata != data and isboi == 1):
+				
+				print("RECIVING DATA "+str(frames))
+				#print(bytearray(data))
 				r.write(bytearray(data))
-				#r.write(data)
-				#newdata = str(data).replace('b',"")
-				#r.write(newdata.replace("'",""))
-			print("done")
-			print(data)
-			
+
 			prevdata = data
-			#break
-			#with open('recived.jpg','rb') as r:
-				#print(r.readlines())
-			#conn.sendall(data)
 			
+			
+		#print("Finished")
