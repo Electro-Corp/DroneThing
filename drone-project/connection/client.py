5eb7e17ab461b3fbfbbf2f8a8f73c9f39641cc87
Electro <chinmaytanmay@outlook.com>
2022-08-07 15:27:45 -0600

    lots of images for video lets go

--- drone-project/connection/client.py
+++ drone-project/connection/client.py
@@ -13,7 +13,9 @@ except:
 
 HOST = '192.168.86.35'
 print("Connecting to: "+HOST)
-PORT = 8080
+PORT = 6000
+resodiv = 1
+frames = 50
 try:
     camera = PiCamera()
     camera.resolution = (1024,720)
@@ -23,20 +25,42 @@ except:
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     #while True:
-    for b in range(2):
+    for g in range(1):
         print("taking and sending...")
         try:
-            camera.capture('test.jpg')
+            camera.capture('img.jpg')
         except:
-            ret, image = cam.read()
-            
-            cv2.imwrite('/home/segfault/DroneThing/drone-project/connection/test.jpg',image)
-            
-        with open('test.jpg',"rb") as image:
-            f = image.read()
-            b = bytearray(f)
-            s.sendall(b)
+            cam.set(cv2.CAP_PROP_FRAME_WIDTH,480/resodiv)
+            cam.set(cv2.CAP_PROP_FRAME_HEIGHT,360/resodiv)
+            #based = 0
+            for i in range(frames):
+                ret, image = cam.read()
+                try:
+                    cv2.imwrite('/home/segfault/DroneThing/drone-project/connection/img'+str(i)+'.jpg',image)
+                except:
+                    print("Failed at: " + str(i))
+                print("Took: "+str(i))
+
+        for i in range(frames):
+            with open('img'+str(i)+'.jpg',"rb") as image:
+                f = image.read()
+                b = bytearray(f)
+ 
+                s.sendall(b)
+                print("Sending: "+str(i))
+                time.sleep(0.01)
+                based = 0xf8
+                
+                s.sendall(bytearray([based]))
+                
+                print("Confirm bit: "+ str(bytearray([based])))
+                count = 0
+                time.sleep(0.01)
+                
+                
         print("Done!")
         print(len(b))
+        #based = bytearray([0x00])
+        #s.sendall(based)
         time.sleep(0.4)
 cam.release()
