3d3be369f3f7468c2b72505198dae640b3dfde67
Electro-Corp <chinmaytanmay@outlook.com>
2022-08-10 08:02:27 -0600

    Added

--- drone-project/connection/server.py
+++ drone-project/connection/server.py
@@ -6,6 +6,7 @@ PORT = 6000
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.bind((HOST,PORT))
 	s.listen(1)
+	#s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSERADDR,1)
 	conn, addr = s.accept()
 	#data = conn.recv(350000)
 	#prevdata = data
@@ -17,8 +18,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	
 	#with conn:
 	frames = 0
+	print("Connection: ",addr)
+	i = 0
+	parts = []
 	while True:
-		#print("Connection: ",addr)
+		
 		while True:
 			
 			#print("reciving")
@@ -30,23 +34,34 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		#		data = conn.recv(10000)
 			#print("done")
 			if not data:
+				print("AVERAGE: "+str(sum(parts)/len(parts)))
 				frames = 0 
 				break
 			#print("writing")
 			#os.system("rm recived.jpg")
 			b = bytearray(data)
-			i = 0
-
+			
+			#print(bytearray(data))
+			#print(data)
 			if(data == bytearray([0xf8])):
 				print("NEXT FRAME")
 				frames = frames + 1
+				if ( i != 0):
+                                	parts.append(i)
+				i = 0
+				
 				break		
-			
+			#print(str(b[len(b)-1]))
+			#if(b[len(b)-1] ==248):
+		#		print("NEXT FRAME REAL")
+	#			frames = frames +1
+#				break
 			with open('images/recived'+str(frames)+'.jpg','ab') as r:
 					#if(prevdata != data and isboi == 1):
 				
-				print("RECIVING DATA "+str(frames))
-				#print(bytearray(data))
+				print("RECIVING DATA "+str(frames)+" PART: "+str(i))
+				i = i + 1
+				
 				r.write(bytearray(data))
 
 			prevdata = data
